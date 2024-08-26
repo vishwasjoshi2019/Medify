10439b3dd549ba78c989b5467632a067fa3ff2a9
Vishwas Joshi <vishwas.j@iitgn.ac.in>
2024-08-26 14:00:48 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Outlet } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import Footer from "./components/Footer/Footer";
@@ -11,28 +10,6 @@ function App() {
       <Outlet />
       <DownloadApp />
       <Footer />
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> f99e1ce5735e7796d3f6893a7eefcbcba9f608dd
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -145,22 +144,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> f99e1ce5735e7796d3f6893a7eefcbcba9f608dd
